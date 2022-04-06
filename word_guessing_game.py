f73b1b9fd91dd8d57ec6233c6200cb5bb74f9306
charlierehder <charlesreh1577@gmail.com>
2022-04-06 11:49:02 -0500

    completed core gameplay loop

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -1,9 +1,33 @@
 import random
 
-word_filename = "words.txt"
-
+# helper function to get random words from dictionary
 def get_word():
-
+    word_filename = "words.txt"
     with open(word_filename, "r") as word_file:
         return random.choice(word_file.readlines()).strip()
+
+
+# setup gameplay loop by getting a random word
+# and setting up a display string
+correct_word = get_word()
+display = ["_"] * len(correct_word)
+turn_limit = 7
+
+for i in range(turn_limit):
+
+    guess = str(input("Guess: "))
+    
+
+    # single letter guess
+    if len(guess) == 1:
+        for i,v in enumerate(correct_word):
+            if v == guess:
+                display[i] = v
+        print("".join(display))
+
+    # guess full word
+    else:
+        if guess == correct_word:
+            print("You win!")
+            break
 
